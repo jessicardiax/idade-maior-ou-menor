76908fb6b23c3b67d39694777fe0a22d05f803e1
Jéssica Dias <110692688+jessicardiax@users.noreply.github.com>
2022-10-15 19:13:55 -0300

    Add files via upload

---
+++ main (3).c
@@ -0,0 +1,21 @@
+//Faça uma função, sem retorno, que recebe a idade do usuário por parâmetro, verifica
+//e informa se ele é maior de idade ou não.
+#include <stdio.h>
+void idade(){
+int i,idade;
+printf("digite sua idade:");
+scanf("%d",&idade);
+if (idade<=18){
+    printf("menor de idade!");
+}
+
+else{
+(idade>17);
+    printf("maior de idade!");
+}
+}
+int main(){
+idade();
+}
+
+
